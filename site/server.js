3324cddb243a500774aa8d69a84b29715c92261a
Gabriel Ferreira <43641763+GFerreira03@users.noreply.github.com>
2020-06-01 16:06:43 -0300

    login

--- site/server.js
+++ site/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoClient = require('mongodb').MongoClient;
-const mongoose = require('mongoose');
+
 const uri = 'mongodb+srv://gabrielfas:011235813@cluster0-vmxrj.gcp.mongodb.net/test?retryWrites=true&w=majority';
 
 mongoClient.connect(uri, {
@@ -12,6 +12,7 @@ mongoClient.connect(uri, {
     console.log('Conectado ao banco de dados');
     //Constante do banco de dados e coleções
     const db = client.db('e-stantedb');
+    
     const usersCollection = db.collection('usuarios');
     const booksCollection = db.collection('livros');
     const salesCollection = db.collection('vendas');
@@ -19,13 +20,8 @@ mongoClient.connect(uri, {
     app.set('view engine', 'ejs');
     app.use(bodyParser.urlencoded({ extended: true}));
     app.use('/static', express.static('public'));
-    app.listen(8000, function(){
+    app.listen(3000, function(){
         console.log('Listening on 3000')
-        /*usersCollection.find({email:"gabrielferreira1d2015@gmail.com"}).project({email:1, _id: 0}).toArray(function(err, result){
-            if (err) throw err;
-            var x = result;
-            console.log(x);
-        })*/
     });
 
     app.get('/', (req, res) => {
@@ -49,4 +45,26 @@ mongoClient.connect(uri, {
         })
         .catch(error => console.error(error))
     });
+
+    app.post('/logar', (req, res) => {
+        var data = {
+            email: req.body.email,
+            senha: req.body.senha
+        };
+        usersCollection.findOne(data, function(err, result){
+            if(err) {
+                res.json({
+                    status:0,
+                    message: err
+                });
+            } if (!result) {
+                res.json({
+                    status: 0,
+                    msg: "Não encontrado"
+                });
+            }
+            res.redirect('/index')
+        })
+    });
+
 }).catch(console.error);
